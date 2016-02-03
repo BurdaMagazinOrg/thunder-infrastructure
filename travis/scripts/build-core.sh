73425189773fa59171b1d1e851aa4352373ae834
Daniel Bosen <daniel@bosen.eu>
2016-02-03 16:51:38 +0100

    move drush msha into travis.yml

--- travis/scripts/build-core.sh
+++ travis/scripts/build-core.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-MAKEFILE_HASH="$(drush msha thunder.yml --porcelain=true)"
 ARTIFACTS_CORE_PATH="${HOME}/artifacts/core"
 FILE_PATH="${ARTIFACTS_CORE_PATH}/${MAKEFILE_HASH}.tar.gz"
 
