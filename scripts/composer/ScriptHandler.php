6e000c0f531d801b40d3753cbbf35f0efe8e3810
Daniel Bosen <daniel.bosen@burda.com>
2016-08-16 09:31:10 +0200

    fix directory mod

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -42,8 +42,10 @@ class ScriptHandler {
       }
     }
 
+
     // Prepare the settings file for installation
     if (!$fs->exists($root . '/sites/default/settings.php')) {
+      $fs->chmod($root . '/sites/default/', 0755);
       $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
       $fs->chmod($root . '/sites/default/settings.php', 0666);
       $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
@@ -51,6 +53,7 @@ class ScriptHandler {
 
     // Prepare the services file for installation
     if (!$fs->exists($root . '/sites/default/services.yml')) {
+      $fs->chmod($root . '/sites/default/', 0755);
       $fs->copy($root . '/sites/default/default.services.yml', $root . '/sites/default/services.yml');
       $fs->chmod($root . '/sites/default/services.yml', 0666);
       $event->getIO()->write("Create a sites/default/services.yml file with chmod 0666");
